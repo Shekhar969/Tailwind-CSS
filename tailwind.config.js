6d8a7659d0a7a6fe776f7e212ee4e1e1019a81fd
Shekhar969 <shekharrawal96@gmail.com>
2024-12-22 20:14:58 +0545

    Tailwind Dirs Installed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html", // Include the HTML files
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS, JSX, TS, and TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
